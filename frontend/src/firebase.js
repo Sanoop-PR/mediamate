b315722b131e8994bb936813b77fcc2c3ee3c77b
SANOOP PR <80612964+Sanoop-PR@users.noreply.github.com>
2024-02-11 10:46:55 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,12 +3,12 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDLKbBg91MMkW8j7WIMM1EDm9wA7_Z8vOU",
-  authDomain: "mediamate-411214.firebaseapp.com",
-  projectId: "mediamate-411214",
-  storageBucket: "mediamate-411214.appspot.com",
-  messagingSenderId: "64739718401",
-  appId: "1:64739718401:web:f7fd82315cfe45acd91700"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
  
 export const app = initializeApp(firebaseConfig);
