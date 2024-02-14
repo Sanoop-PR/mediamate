60d417b2066fd19ada29d2a64605c83dfe44793e
SANOOP PR <80612964+Sanoop-PR@users.noreply.github.com>
2024-02-14 10:41:07 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -36,8 +36,8 @@ function Login() {
   function handleGoogleAuth(){
     const rootUrl = "https://accounts.google.com/o/oauth2/v2/auth";
     const options = {
-      redirect_uri:process.env.REACT_APP_GOOGLE_OAUTH_REDIRECT_URL,
-      client_id:process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      redirect_uri:import.meta.env.REACT_APP_GOOGLE_OAUTH_REDIRECT_URL,
+      client_id:import.meta.env.REACT_APP_GOOGLE_CLIENT_ID,
       
       access_type:"offline",
       response_type:"code",
